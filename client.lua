cb9e209b18db04c06220234e896fcbea047e7222
CodeWizard245 <codewizard245@yahoo.com>
2024-10-17 09:20:38 +0300

    Add files via upload

--- client.lua
+++ client.lua
@@ -24,3 +24,38 @@ RegisterCommand('dv', function(source, args, rawCommand)
         })
     end
 end, false)
+
+TriggerEvent('chat:addSuggestion', '/dv', 'Remove the nearest vehicle.')
+
+RegisterCommand('cv', function(source, args, rawCommand)
+    local vehicleName = args[1]
+    if vehicleName == nil then
+        TriggerEvent('chat:addMessage', {
+            color = { 255, 0, 0 },
+            multiline = true,
+            args = { "Error", "You did not specify the name of the vehicle." }
+        })
+        return
+    end
+
+    local playerPed = GetPlayerPed(-1)
+    local playerCoords = GetEntityCoords(playerPed)
+
+    RequestModel(vehicleName)
+    while not HasModelLoaded(vehicleName) do
+        Wait(500)
+    end
+
+    local vehicle = CreateVehicle(vehicleName, playerCoords.x + 5, playerCoords.y + 5, playerCoords.z, GetEntityHeading(playerPed), true, false)
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+
+    TriggerEvent('chat:addMessage', {
+        color = { 0, 255, 0 },
+        multiline = true,
+        args = { "System", "The vehicle has been created: " .. vehicleName }
+    })
+
+    SetModelAsNoLongerNeeded(vehicleName)
+end, false)
+
+TriggerEvent('chat:addSuggestion', '/cv', 'Create a vehicle. Use: /cv [the name of the transport vehicle]')
